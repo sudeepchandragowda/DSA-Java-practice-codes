13809b6b9c7247941e8c27c1e63dc5245cc7a897
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-06-29 03:40:05 +0530

    Added PrintSumSubArray code

--- src/practiseData/Client.java
+++ src/practiseData/Client.java
@@ -1,8 +1,18 @@
 package practiseData;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Client {
     public static void main(String[] args) {
         IntegerPrinter integerPrinter = new IntegerPrinter(5);
         integerPrinter.print();
+
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1,2,3,4));
+        PrintSumSubArray printSumSubArray = new PrintSumSubArray();
+        int result = printSumSubArray.sumOfSubArray(A);
+        System.out.println(result);
     }
 }
--- src/practiseData/PrintSumSubArray.java
+++ src/practiseData/PrintSumSubArray.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class PrintSumSubArray {
     public int sumOfSubArray(ArrayList<Integer> A) {
         int n = A.size();
-        int sum = 0;
+        int ans = 0;
         for(int i=0; i<n; i++) {
+            int sum = 0;
             for(int j=i; j<n; j++) {
                 sum += A.get(j);
+                ans += sum;
             }
         }
-        return sum;
+        return ans;
     }
 }
