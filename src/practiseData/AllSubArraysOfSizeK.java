a2a8557268d7e0b0ea1e7b910f53d53bebdebc82
Sudeep Chandra B S <122792474+sudeepchandragowda@users.noreply.github.com>
2024-07-07 20:47:01 +0530

    Added comment for the method AllSubArraysOfSizeK

--- src/practiseData/AllSubArraysOfSizeK.java
+++ src/practiseData/AllSubArraysOfSizeK.java
@@ -7,10 +7,12 @@ public class AllSubArraysOfSizeK {
         int n = A.size();
         int s = 0;
         int e = k-1;
-        while (e<n && s<=n-k) {
+        while (e<n) {
             System.out.println(s + " " + e);
             s++;
             e++;
         }
     }
 }
+
+//Either one condition from while(e<n) or while (s<n-k) or while (e<n && s<n-k) can work for while loop
